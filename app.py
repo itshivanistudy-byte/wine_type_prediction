26e14fd99e6199d9bf49cc2a558c8ce318608560
itshivanistudy-byte <itshivani.study@gmail.com>
2026-01-11 15:46:20 +0530

    app.py

--- app.py
+++ app.py
@@ -1,35 +1,33 @@
 # -*- coding: utf-8 -*-
+"""wine_deployment.ipynb"""
 
+#import libraries
 import numpy as np
 import joblib
 import streamlit as st
 
-# Load model
-model = joblib.load("wine_type_prediction.pkl")
+#wine_type_prediction.pkl
+model = joblib.load('wine_type_prediction.pkl')
 
-# Page config
-st.set_page_config(page_title="Wine Type Prediction", layout="centered")
-
+st.set_page_config(page_title='Wine Type Prediction', layout="centered")
 st.title("Wine Type Classification App")
-st.write("Predict whether the wine is **Red** or **White** using chemical properties")
+st.write("predict whether the wine is **red** or **white** using chemical properties")
 
-# Inputs
-fixed_acidity = st.number_input("Fixed Acidity", min_value=0.0)
-volatile_acidity = st.number_input("Volatile Acidity", min_value=0.0)
-citric_acid = st.number_input("Citric Acid", min_value=0.0)
-residual_sugar = st.number_input("Residual Sugar", min_value=0.0)
-chlorides = st.number_input("Chlorides", min_value=0.0)
-free_sulfur_dioxide = st.number_input("Free Sulfur Dioxide", min_value=0.0)
-total_sulfur_dioxide = st.number_input("Total Sulfur Dioxide", min_value=0.0)
-density = st.number_input("Density", min_value=0.0)
-pH = st.number_input("pH", min_value=0.0)
-sulphates = st.number_input("Sulphates", min_value=0.0)
-alcohol = st.number_input("Alcohol", min_value=0.0)
-quality = st.number_input("Quality", min_value=0.0)
+fixed_acidity = st.number_input("value of fixed acidity")
+volatile_acidity = st.number_input("value of volatile acidity")
+citric_acid = st.number_input("value of citric acid")
+residual_sugar = st.number_input("value of residual sugar")
+chlorides = st.number_input("value of chlorides")
+free_sulfur_dioxide = st.number_input("value of free sulfur dioxide")
+total_sulfur_dioxide = st.number_input("value of total sulfur dioxide")
+density = st.number_input("value of density")
+pH = st.number_input("value of pH")
+sulphates = st.number_input("value of sulphates")
+alcohol = st.number_input("value of alcohol")
+quality = st.number_input("value of quality")
 
-# Prediction
 if st.button("Predict"):
-    input_data = np.array([[ 
+    input_data = np.array([[
         fixed_acidity,
         volatile_acidity,
         citric_acid,
@@ -47,7 +45,6 @@ if st.button("Predict"):
     prediction = model.predict(input_data)[0]
 
     if prediction == 1:
-        st.success("White Wine")
+        st.success("**white wine**")
     else:
-        st.error("Red Wine")
-
+        st.error("**red wine**")
